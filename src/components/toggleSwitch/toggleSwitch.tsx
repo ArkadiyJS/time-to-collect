d4a1c7b755f683304e272d3705a5e115d514cf1f
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2023-04-18 08:43:10 +0500

    create skellet app + create toggle components (admin panel)

---
+++ src/components/toggleSwitch/toggleSwitch.tsx
@@ -0,0 +1,20 @@
+import s from './toggleSwitch.module.css'
+
+function ToggleSwitch({label,setShowButtonCreateList,showButtonCreateList}) {
+  return (
+    <div className={s.container}>
+      {label}{" "}
+    <div className={s.toggleSwitch}>
+        <input onClick={()=>{setShowButtonCreateList(!showButtonCreateList)}}
+        type="checkbox" className={s.checkbox} 
+               name={label} id={label} />
+        <label className={s.label} htmlFor={label}>
+          <span className={s.inner} />
+          <span className={s.switch} />
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default ToggleSwitch;
