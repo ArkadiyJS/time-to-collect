18b299c1c8d24f7acc6fd84487a570138a9eed91
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2023-11-01 21:18:20 +0500

    create new page

---
+++ src/components/notificationPage/notificationPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+// уведомления которые будет видить только админ
+function NotificationPage(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default NotificationPage;
---
+++ src/components/profilePage/profilePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+// профиль юзера обдумать структуру (кол-во уп. время. даты.  и тд и тп)
+function ProfilePage(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ProfilePage;
